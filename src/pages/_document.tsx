a1056d7cdf212bc8f25fc7f18528bea7ab21c1cb
rodrigosuelli <rodrigosuelli@gmail.com>
2021-11-17 10:24:26 -0300

    Start developing login page

---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="Ferramenta de controle de estoque e vendas."
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ src/pages/login.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Hardshop - Entrar</title>
+      </Head>
+
+      <h1>Login page!</h1>
+    </>
+  );
+};
+
+export default Login;
